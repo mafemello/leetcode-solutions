0291071281ab92e5dcbf785507273e4f3e048fb2
guilhermehiromoto <guilherme.hiromoto@windowslive.com>
2021-10-28 16:30:19 -0300

    adding two sum

---
+++ Two Sum/two_sum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i,num in enumerate(nums):
+            if num in d:
+                return [i,d[num]]
+            d[target-num] = i
